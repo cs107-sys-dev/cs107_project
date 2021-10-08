44452b2188272f4d15f927ebaf59404c78534a56
Fabian Wermelinger <cs107.sys.dev@gmail.com>
2021-10-08 10:55:50 -0400

    More flexible test driver

--- tests/run_coverage.sh
+++ tests/run_coverage.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
+# File       : run_coverage.sh
+# Description: Coverage wrapper around test suite driver script
+# Copyright 2021 Harvard University. All Rights Reserved.
 set -e
 
-# run the tests (generates coverage data to build report)
-./run_tests.sh coverage run --source=.,cs107_package
+tool='coverage'
+if [[ $# -eq 1 ]]; then
+    tool="${1}"; shift
+fi
 
-# build the coverage report in xml format
-coverage xml
+if [[ ${tool} == 'coverage' ]]; then
+    # run the tests (generates coverage data to build report)
+    ./run_tests.sh coverage run --source=.,cs107_package "${@}"
+
+    # build the coverage report in xml format
+    coverage xml
+elif [[ ${tool} == 'pytest' ]]; then
+    # generate coverage reports with pytest in one go
+    ./run_tests.sh pytest --cov=cs107_package --cov-report=xml "${@}"
+else
+    >&2 echo "Error: unknown tool '${tool}'"
+    exit 1
+fi
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env bash
+# File       : run_tests.sh
+# Description: Test suite driver script
+# Copyright 2021 Harvard University. All Rights Reserved.
 set -e
 
-driver=''
-if [[ $# -eq 0 ]]; then
-    driver='python'
-else
-    driver="$@"
-fi
+# list of test cases you want to run
+tests=(
+    # test_other_things_on_root_level.py
+    subpkg_1/test_module_1.py
+    # subpkg_2/test_module_3.py
+    # subpkg_2/test_module_4.py
+)
+
+# decide what driver to use (depending on arguments given)
+unit='-m unittest'
+driver="python ${unit}"
+for arg; do
+    case ${arg} in
+        coverage) driver="${@} ${unit}"; break;;
+        pytest*) driver="${@}"; break;;
+    esac
+done
 
 # we must add the module source path because we use `import cs107_package` in
 # our test suite and we want to test from the source directly (not a package
-# that we have possibly installed earlier)
+# that we have (possibly) installed earlier)
 export PYTHONPATH="$(pwd -P)/../src":${PYTHONPATH}
-export PYTHONPATH="$(pwd -P)/subpkg_1":${PYTHONPATH}
 
 # run the tests
-${driver} -m unittest subpkg_1/test_module_1.py
+${driver} ${tests[@]}
